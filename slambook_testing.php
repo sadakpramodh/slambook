d0c88f63c30a6146ef35c64e6617cc4d84d26365
sadak pramodh <sadakpramodh@live.com>
2014-07-02 08:46:18 +0530

    Create slambook_testing.php

---
+++ slambook_testing.php
@@ -0,0 +1,11 @@
+<script src="../SpryAssets/SpryCollapsiblePanel.js" type="text/javascript"></script>
+<link href="../SpryAssets/SpryCollapsiblePanel.css" rel="stylesheet" type="text/css" />
+<div id="CollapsiblePanel1" class="CollapsiblePanel">
+  <div class="CollapsiblePanelTab" tabindex="0"><center>projects</center></div>
+  <div class="CollapsiblePanelContent">slambook</div>
+</div>
+<div class="CollapsiblePanelContent">slambook</div>
+</div>
+<script type="text/javascript">
+var CollapsiblePanel1 = new Spry.Widget.CollapsiblePanel("CollapsiblePanel1");
+</script>
